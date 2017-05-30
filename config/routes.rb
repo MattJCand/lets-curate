e0671b24cbb97e2d8df269bb2afca43c3d67d2b2
Aurore Courtois <aurore.courtois@outlook.fr>
2017-05-30 16:00:10 +0200

    Added user views and pundit for users and team index view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  resources :users, only: [ :show, :edit, :update ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :teams do
     resources :members, only: [ :create ]
---
+++ db/migrate/20170530122846_add_art_types_to_users.rb
@@ -0,0 +1,5 @@
+class AddArtTypesToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :art_types, :string
+  end
+end
